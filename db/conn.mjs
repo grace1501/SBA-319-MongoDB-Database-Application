c062a79d07475c676aa146c280f90227de24af8e
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-01-27 20:59:47 -0600

    basic functions done

---
+++ db/conn.mjs
@@ -0,0 +1,20 @@
+import { MongoClient } from 'mongodb'
+
+const connectionString = process.env.ATLAS_URI
+
+const client = new MongoClient(connectionString)
+
+let conn;
+
+try {
+    conn = await client.connect();
+
+    console.log("Successfully connected to MongoDB")
+
+} catch(e) {
+    console.log(e)
+}
+
+let db = conn.db('sample_training');
+
+export default db;
---
+++ server.mjs
@@ -0,0 +1,14 @@
+import express from 'express'
+import 'dotenv/config'
+
+const PORT = process.env.PORT || 5050
+const app = express()
+
+app.use(express.json())
+
+import grades from './routes/grades.mjs';
+app.use('/grades', grades)
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port: ${PORT}`)
+})
